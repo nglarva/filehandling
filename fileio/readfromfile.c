bf0f5d739aced8803e844a3edd0e5d8477dd7a57
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-04-10 17:55:10 +0700

    Read file with fgetc fgets

--- fileio/readfromfile.c
+++ fileio/readfromfile.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(void){
     FILE *fptr;
     fptr = fopen("data.txt","r");
-    char c;
+    char name[30];
+    char year[10];
+    int i=0;
+    rewind(fptr);
     if(fptr != NULL){
-        c = fgetc(fptr);
-        printf("%c\n", c);
+       // do {
+            // name[i] = fgetc(fptr);
+            // printf("%c", name[i]);
+            // if(name[i] == '\n'){
+            //     name[i] = '\0';
+            //     break;
+            // }
+            // i++;
+            fgets(name,50,fptr);
+            fgets(year,50,fptr);
+            
+        // }
+        // while(name[i-1] != EOF);
+        //name[i] = '\0';
+        printf("Ten: %s\n",name);
+        printf("Nam sinh: %s\n",year);
     }
     else{
         printf("File khong ton tai\n");
