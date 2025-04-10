177593f33cfd6e86021e8d6c353f906f9104bfc4
ngovannamt36@gmail.com <ngovannamt36@gmail.com>
2025-04-10 18:18:08 +0700

    fscanf, fgets, fgetc

--- fileio/readfromfile.c
+++ fileio/readfromfile.c
@@ -4,7 +4,11 @@ int main(void){
     FILE *fptr;
     fptr = fopen("data.txt","r");
     char name[30];
-    char year[10];
+    //char year[10];
+    int year;
+    float weight;
+    //char weight[10];
+    char random[10];
     int i=0;
     rewind(fptr);
     if(fptr != NULL){
@@ -16,14 +20,26 @@ int main(void){
             //     break;
             // }
             // i++;
-            fgets(name,50,fptr);
-            fgets(year,50,fptr);
+            // fgets(name,50,fptr);
+            // fgets(year,50,fptr);
+            // fgets(weight,50,fptr);
+            fscanf(fptr,"%[^\n]",name);
+            fscanf(fptr,"%d",&year);
+            fscanf(fptr,"%f",&weight);
+
+            
             
         // }
         // while(name[i-1] != EOF);
         //name[i] = '\0';
         printf("Ten: %s\n",name);
-        printf("Nam sinh: %s\n",year);
+        printf("Nam sinh: %d\n",year);
+        printf("Can nang: %.2f\n",weight);
+        if(fgets(random,50,fptr) ==NULL)
+            {
+                printf("End of file");
+            }
+            
     }
     else{
         printf("File khong ton tai\n");
